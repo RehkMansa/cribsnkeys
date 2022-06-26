3f6b1f30bb643cfdc52dc7e8936b9823da675397
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-26 20:31:01 +0100

    added left wrapper and bgImage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,13 +2,31 @@ import styled from 'styled-components';
 
 const Wrapper = styled.section`
   display: flex;
+  & > div {
+    width: 50%;
+  }
 `;
-const LeftContainer = styled.div``;
+const LeftContainer = styled.div`
+  background: url(/images/${(props) => props.bgImage}), rgba(0, 0, 0, 0.3);
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  min-height: 100vh;
+
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+`;
 const RightContainer = styled.div``;
 const HomePage = () => {
   return (
     <Wrapper>
-      <LeftContainer></LeftContainer>
+      <LeftContainer bgImage={'home-bg-left.jpg'}>
+        <div className="overlay"></div>
+      </LeftContainer>
       <RightContainer></RightContainer>
     </Wrapper>
   );
