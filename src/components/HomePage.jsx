f2747bcc66e98788467721ddb7444585501aa396
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-26 20:09:05 +0100

    sectioned the homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section``
+const LeftContainer = styled.div``
+const RightContainer = styled.div``
 const HomePage = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>Hello World</h1> <p>Welcome to my app</p>
-    </div>
+    </Wrapper>
   );
 };
 
