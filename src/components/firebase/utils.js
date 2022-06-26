22d22d55a1fac98a0c0d2d5f1017071e5a78af51
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-26 20:11:24 +0100

    added firebase utils

--- src/components/firebase/utils.js
+++
@@ -1,7 +0,0 @@
-import { firebaseConfig } from './config';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
