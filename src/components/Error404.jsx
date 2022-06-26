be1ae3e0e40df6b9402baf85cdd967117a1b13f9
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-26 20:05:25 +0100

    added error404 page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
   return (
     <div>
       <h3>Page Not found</h3>
+      <Link to={'/'}>Go To Home</Link>
     </div>
   );
 };
