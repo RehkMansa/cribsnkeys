3319f9f29896cdd352b1feed1f5215bae32c91a2
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-27 23:31:43 +0100

    added menuitem component

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,5 +1,29 @@
-const MenuItems = () => {
-  return (  );
-}
- 
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  .menu-title {
+  }
+`;
+
+const MenuItems = ({ icon, title, link }) => {
+  const navigate = useNavigate();
+
+  const redirectTo = () => {
+    navigate(link);
+  };
+
+  return (
+    <Wrapper onClick={redirectTo}>
+      {icon}
+      <div className="menu-title">{title}</div>
+    </Wrapper>
+  );
+};
+
 export default MenuItems;
--- src/components/RightFloatingMenu.jsx
+++ src/components/RightFloatingMenu.jsx
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
+import { FiHome, FiLogIn, FiExternalLink } from 'react-icons/fi';
+import MenuItems from './MenuItems';
+// import { useState } from 'react';
 
 const Wrapper = styled.div``;
 
+const menuItems = [
+  {
+    id: 0,
+    name: home,
+    icon: <FiHome />,
+    link: '/',
+  },
+  {
+    id: 1,
+    name: home,
+    icon: <FiLogIn />,
+  },
+];
+
 const RightFloatingMenu = () => {
+  // const [menu, setMenu] = useState(menuItems);
   return (
     <Wrapper>
-      <MenuItems />
+      {menuItems.map((menu) => (
+        <MenuItems
+          key={menu.id}
+          icon={menu.icon}
+          link={menu.link}
+          title={menu.name}
+        />
+      ))}
+
+      <div>
+        <MenuItems icon={<FiExternalLink />} title={'Sign In'} />
+      </div>
     </Wrapper>
   );
 };
