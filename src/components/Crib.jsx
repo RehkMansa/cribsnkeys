8f8b3beda222be55d1ebd20955295ac7cbbcd1a5
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-29 22:38:12 +0100

    modified the listing file

--- src/components/Crib.jsx
+++ src/components/Crib.jsx
@@ -1,5 +1,9 @@
 const SingleCribs = () => {
-  return ( <div><h4>Single Crib</h4></div> );
-}
- 
+  return (
+    <div>
+      <h4>Single Crib</h4>
+    </div>
+  );
+};
+
 export default SingleCribs;
