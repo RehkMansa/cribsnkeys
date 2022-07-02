723f587fe7490b91fdf6508298b36e77dbce5044
RehkMansa <godspowernathaniel25@gmail.com>
2022-07-02 23:39:23 +0100

    added single crib

--- src/components/SingleCrib.jsx
+++ src/components/SingleCrib.jsx
@@ -1,8 +1,49 @@
-const SingleCrib = () => {
+import { FaMapMarkerAlt, FaStar } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+const AgentCard = styled.div``;
+const ListingDetails = styled.div``;
+const Header = styled.div``;
+const SingleCrib = (props) => {
+  // const { title, imgURL, agent, location } = props;
   return (
-    <div>
-      <h4>Single Crib</h4>
-    </div>
+    <Wrapper>
+      <Header>
+        <div className="details">
+          <div className="header">
+            <h3>Houston Villa, 3 Bedroom Flat</h3>
+            <button>price</button>
+          </div>
+          <div className="details-inner">
+            <div className="icon-holder">
+              <FaMapMarkerAlt />
+              <p>Gbagada, Lagos</p>
+            </div>
+            <div className="icon-holder">
+              <FaStar />
+              <p>Reviews</p>
+            </div>
+          </div>
+        </div>
+      </Header>
+      <ListingDetails>
+        <img src="/images/singe-home.webp" alt="home" />
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+            nulla!
+          </p>
+        </div>
+      </ListingDetails>
+      <AgentCard>
+        <img src="/images/default-user.jpg" alt="user" />
+        <div className="icon-holder">
+          <p>displayName</p>
+          <MdVerified />
+        </div>
+      </AgentCard>
+    </Wrapper>
   );
 };
 
