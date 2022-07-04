a35c1c4cb0d75a1ee940102a34ed787fdd75b2ec
RehkMansa <godspowernathaniel25@gmail.com>
2022-07-04 01:59:20 +0100

    added agents page

---
+++ src/components/AgentSingle.jsx
@@ -0,0 +1,5 @@
+const AgentSingle = () => {
+  return <h4>AgentSingle</h4>;
+};
+
+export default AgentSingle;
---
+++ src/components/AgentsPage.jsx
@@ -0,0 +1,5 @@
+const AgentsPage = () => {
+  return <h3>AgentsPage</h3>;
+};
+
+export default AgentsPage;
